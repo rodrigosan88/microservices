864f9613e03c76efc41d1c49228b2fd8b0752b74
Rodrigo Santos Menezes <rodrigo.menezes@cnj.jus.br>
2017-03-20 12:08:03 -0300

    renomeando pasta do servico de autorizacao

--- auth/src/main/java/br/jus/cnj/pje/autorizacao/service/UsusarioLoginService.java
+++
@@ -1,8 +0,0 @@
-package br.jus.cnj.pje.autorizacao.service;
-
-import br.jus.cnj.pje.autorizacao.model.UsuarioLogin;
-
-public interface UsusarioLoginService {
-	UsuarioLogin findUsuarioLogin(String login);
-	Iterable<UsuarioLogin> listUsuarioLogin();
-}
